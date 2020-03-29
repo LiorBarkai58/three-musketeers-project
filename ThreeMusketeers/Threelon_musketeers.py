0d2e1ff2fc9111e7ea06fa1f526ef5840d4dd11e
lior <lior5858@gmail.com>
2020-03-29 19:43:02 +0300

    Implement menu

--- ThreeMusketeers/Threelon_musketeers.py
+++ ThreeMusketeers/Threelon_musketeers.py
@@ -2,39 +2,52 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.image import Image
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
 from ThreeMusketeers.Board import Board, GraphicBoard
 
 
 board = Board()
-graphicGrid = GraphicBoard(board, "M")
-graphicGrid.initialize_board()
+graphicGridGAI = GraphicBoard(board, "G")
+graphicGridGAI.initialize_board()
 
-Builder.load_string(""" 
-<Menu>: 
-    BoxLayout: 
-        Button: 
-            text: "Play" 
-            background_color : 0, 0, 1, 1 
-            on_press: 
-                # You can define the duration of the change 
-                # and the direction of the slide 
-                root.manager.transition.direction = 'left' 
-                root.manager.transition.duration = 1 
-                root.manager.current = 'Game'
-        Button: 
-            text: "Rules" 
-            background_color : 0, 0, 1, 1 
-            on_press: 
-                # You can define the duration of the change 
-                # and the direction of the slide 
-                root.manager.transition.direction = 'left' 
-                root.manager.transition.duration = 1 
-                root.manager.current = 'rules' 
+board2 = Board()
+graphicGridMAI = GraphicBoard(board2, "M")
+graphicGridMAI.initialize_board()
 
-<rules>: 
-    GridLayout: 
+Builder.load_string("""
+<Menu>:
+    BoxLayout:
+        orientation: 'vertical'
+        BoxLayout:
+            orientation: 'horizontal'
+            Button:
+                text: "Play as musketeers"
+                font_size: 25
+                background_color : 0, 0, 15, 15
+                on_press:
+                    # You can define the duration of the change
+                    # and the direction of the slide
+                    root.manager.current = 'GameGAI'
+            Button:
+                text: "Play as guardsmen"
+                font_size: 25
+                background_color : 0, 0, 15, 15
+                on_press:
+                    # You can define the duration of the change
+                    # and the direction of the slide
+                    root.manager.current = 'GameMAI'
+        Button:
+            text: "Rules"
+            font_size: 40
+            background_color : 0, 0, 15, 15
+            on_press:
+                # You can define the duration of the change
+                # and the direction of the slide
+                root.manager.current = 'rules'
+
+<rules>:
+    BoxLayout:
         Button:
             text: "Back to menu"
             center_x: 50
@@ -42,43 +55,58 @@ Builder.load_string("""
             size: 100, 100
             id: rtrn
             on_press:
-                root.manager.transition.direction = 'right'
                 app.root.current = 'Menu'
-            Image:
-                source: "Pics\\guard.png"
-                center_x: 50
-                center_y: 50
-                size: 100, 100
+        Button:
+            text: "Winning Conditions"
+            center_x: 750
+            center_y: 50
+            size: 100, 100
+            on_press:
+                app.root.current = 'winning'
+<winning>:
+    BoxLayout:
+        Button:
+            text: "Back to menu"
+            center_x: 50
+            center_y: 50
+            size: 100, 100
+            id: rtrn
+            on_press:
+                app.root.current = 'Menu'
 
-<Game>: 
-    BoxLayout: 
-        Button: 
-            text: "Go to Screen 4" 
-            background_color : 1, 0, 1, 1 
-            on_press: 
-                root.manager.transition.direction = 'left' 
-                root.manager.transition.duration = 1 
-                root.manager.current = 'Menu' 
+
 """)
 
 class Game(Screen):
-
     pass
 class rules(Screen):
     pass
 class Menu(Screen):
     pass
+class winning(Screen):
+    pass
 
-# Menu = Screen(name='Menu')
-# Game = Screen(name='Game')
-# rules = Screen(name='rules')
 
-sm = ScreenManager()
+sm = ScreenManager(transition=NoTransition())
 menu = Menu(name="Menu")
 rules = rules(name="rules")
+rules.add_widget(Image(source="Pics\\rules.png"))
+
+winning = winning(name="winning")
+winning.add_widget(Image(source="Pics\\winningcon.png"))
+
+gameGAI = Game(name="GameGAI")
+gameGAI.add_widget(graphicGridGAI)
+
+gameMAI = Game(name="GameMAI")
+gameMAI.add_widget(graphicGridMAI)
+
+
 sm.add_widget(menu)
-sm.add_widget(Game(name="Game"))
+sm.add_widget(gameGAI)
+sm.add_widget(gameMAI)
 sm.add_widget(rules)
+sm.add_widget(winning)
 
 
 
@@ -93,12 +121,5 @@ class TestApp(App):
 
 
 TestApp().run()
-
-
-while not board.game_over:
-    board.print_board()
-    board.move_musk(input())
-    board.print_board()
-    board.move_guard(input())
 
 print("The winning piece was ", board.winning_piece)
